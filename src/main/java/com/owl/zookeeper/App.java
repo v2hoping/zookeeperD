c2b679b004e3b1d55992cedaee99b8f1cc030674
houping wang <whp1473@163.com>
2018-03-13 10:32:08 +0800

    App.java

--- src/main/java/com/owl/zookeeper/App.java
+++ src/main/java/com/owl/zookeeper/App.java
@@ -1,6 +1,5 @@
 package com.owl.zookeeper;
 
-import java.nio.file.WatchKey;
 
 /**
  * Hello world!
@@ -8,7 +7,7 @@ import java.nio.file.WatchKey;
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main(String[] args )
     {
 
         System.out.println( "Hello World!" );
